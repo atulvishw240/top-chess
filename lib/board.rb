51256f6650416ed055137e45481f9e7385d57ff8
atulvishw240 <atulvishw240@gmail.com>
2025-08-07 08:17:06 +0530

    Create an outline for Board#display

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,18 @@
 # Handle Game Effect on Board
 class Board
   attr_accessor :board
-  
+
   def initialize
-    @board = Array.new(8) { Array.new(8, " ") }
+    @board = Array.new(8) { Array.new(8, " Atul ") }
+  end
+
+  def display
+    board.each do |row|
+      row.each do |square|
+        print square
+      end
+
+      puts ""
+    end
   end
 end
