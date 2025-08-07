596249edff4be1dc3e795c442f2a839b0bdebcfb
atulvishw240 <atulvishw240@gmail.com>
2025-08-07 09:50:41 +0530

    Let Square choose color for itself

--- lib/square.rb
+++ lib/square.rb
@@ -1,6 +1,24 @@
 # Board is made up of squares
 class Square
+  WHITE_BACKGROUND = "\e[47m".freeze
+  CYAN_BACKGROUND = "\e[48;5;45m".freeze
+  RESET_TERMINAL = "\e[0m".freeze
+
+  attr_accessor :color
+
   def to_s
     "Square"
+  end
+
+  def assign_color(row_index, col_index)
+    sum = add(row_index, col_index)
+    self.color = WHITE_BACKGROUND if sum.even?
+
+    self.color = CYAN_BACKGROUND
+  end
+
+  # ------------------------- PRIVATE METHODS -------------------------------
+  def add(row_index, col_index)
+    return row_index + col_index
   end
 end
