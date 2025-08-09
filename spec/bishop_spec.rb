dc477bd5847d60f9b5edc11c288dcfbea7c8cb4a
atulvishw240 <atulvishw240@gmail.com>
2025-08-09 15:58:23 +0530

    Create second test for Bisop#all_possible_moves

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -9,5 +9,13 @@ describe Bishop do
       moves = bishop.all_possible_moves
       expect(moves).to eq(poss_moves)
     end
+
+    it "returns left diagonal moves for Bishop at [0, 7]" do
+      bishop = Bishop.new("Black")
+      bishop.position = Position.new(0, 7)
+      poss_moves = [[1, 6], [2, 5], [3, 4], [4, 3], [5, 2], [6, 1], [7, 0]]
+      moves = bishop.all_possible_moves
+      expect(moves).to eq(poss_moves)
+    end
   end
 end
