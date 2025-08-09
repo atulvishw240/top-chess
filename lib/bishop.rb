2904d094472f0fe0da291fecdfaebbb70523996f
atulvishw240 <atulvishw240@gmail.com>
2025-08-09 15:24:58 +0530

    Calculate upper right diagonal moves for bishop
    Bishop has 2 diagonals and I have divided them further into upper and
    lower.
    Left to Right diagonal => (upper left + lower right)
    Right to Left diagonal => (upper right + lower left)

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -5,4 +5,29 @@ class Bishop
   def initialize(color)
     @color = color
   end
+
+  def all_possible_moves
+    row_index = position[0]
+    col_index = position[1]
+    moves = []
+    loop do
+      row_index = row_index + 1
+      col_index = col_index + 1
+
+      break unless valid_position?(row_index, col_index)
+
+      moves << [row_index, col_index]
+    end
+
+    moves
+  end
+
+  def valid_position?(row_index, col_index)
+    row_index.between?(0, 7) && col_index.between?(0, 7)
+  end
 end
+
+bishop = Bishop.new("Black")
+bishop.position = [2, 4]
+
+p bishop.all_possible_moves
