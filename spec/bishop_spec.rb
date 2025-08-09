78c21e2adb322eb5a47146920bfece34b8900800
atulvishw240 <atulvishw240@gmail.com>
2025-08-09 15:23:59 +0530

    Create first test for all_possible_moves

---
+++ spec/bishop_spec.rb
@@ -0,0 +1,12 @@
+require_relative "../lib/bishop"
+describe Bishop do
+  describe "#all_possible_moves" do
+    it "returns right diagonal moves for Bishop at [0, 0]" do
+      bishop = Bishop.new("Black")
+      bishop.position = [0, 0]
+      poss_moves = [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7]]
+      moves = bishop.all_possible_moves
+      expect(moves).to eq(poss_moves)
+    end
+  end
+end
