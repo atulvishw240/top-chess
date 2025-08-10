b82aafcabd5ac4dc49adde78f9aedc0f885ca383
atulvishw240 <atulvishw240@gmail.com>
2025-08-10 21:02:31 +0530

    Initialize King

--- lib/king.rb
+++ lib/king.rb
@@ -3,4 +3,11 @@ require_relative "position"
 require_relative "piece"
 # Represents King
 class King
+  attr_accessor :position
+  attr_reader :color, :unicode
+
+  def initialize(color)
+    @color = color
+    @unicode = "\u{265A}"
+  end
 end
