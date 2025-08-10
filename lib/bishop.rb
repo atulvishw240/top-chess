4ad4af9b9360fcc807fbabfee7f4cdab1b4c999b
atulvishw240 <atulvishw240@gmail.com>
2025-08-10 20:52:30 +0530

    Bishop can now capture opponent's pieces
    Bishop can make all possible moves now. So our method all_possible_moves
    is now complete

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -19,24 +19,36 @@ class Bishop < Piece
     moves = []
 
     DIAGONALS_DIR.each do |diagonal|
-      calculate_moves_for_one_diagonal(moves, diagonal)
+      moves += calculate_moves_for_one_diagonal(board, diagonal)
     end
 
     moves
   end
 
-  def calculate_moves_for_one_diagonal(moves, diagonal)
-    row_index = position.row
-    col_index = position.col
+  def calculate_moves_for_one_diagonal(board, diagonal) # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
+    moves = []
 
-    loop do
+    row_index = diagonal[0] + position.row
+    col_index = diagonal[1] + position.col
+    position = Position.new(row_index, col_index)
+
+    while position.valid?
+      move = [row_index, col_index]
+
+      if board.contains_piece?(position)
+        piece = board.get_piece(position)
+        moves << move unless piece.color == color
+        break
+      end
+
+      moves << move
+
       row_index += diagonal[0]
       col_index += diagonal[1]
-
-      break unless Position.new(row_index, col_index).valid?
-
-      moves << [row_index, col_index]
+      position = Position.new(row_index, col_index)
     end
+
+    moves
   end
 
   def to_s
