31737b63df81022518b07312195165a2a8c7d265
atulvishw240 <atulvishw240@gmail.com>
2025-08-11 11:47:01 +0530

    King can move and capture opponent's pieces

--- lib/king.rb
+++ lib/king.rb
@@ -6,8 +6,32 @@ class King
   attr_accessor :position
   attr_reader :color, :unicode
 
+  # [top_left, front, top_right, left, right, bottom_left, down, bottom_right]
+  MOVES_DIR = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]].freeze
+
   def initialize(color)
     @color = color
     @unicode = "\u{265A}"
+  end
+
+  def all_possible_moves(board)
+    MOVES_DIR.map { |dir| calculate_move(board, dir) }.compact
+  end
+
+  def calculate_move(board, dir)
+    row_index = position.row + dir[0]
+    col_index = position.col + dir[1]
+    move = [row_index, col_index]
+
+    position = Position.new(row_index, col_index)
+
+    return unless position.valid?
+
+    if board.contains_piece?(position)
+      piece = board.get_piece(position)
+      return if piece.color == color
+    end
+
+    move
   end
 end
