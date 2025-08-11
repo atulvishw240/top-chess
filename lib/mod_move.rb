846036945ef046135c5e90402cd1a21a14487d81
atulvishw240 <atulvishw240@gmail.com>
2025-08-11 23:27:10 +0530

    Extract common method from knight and king
    Knight and king doesn't follow specific patterns like Bishop and Rook,
    where we keep calculating moves in a particular direction unless we
    reach out of the board or get interrupted by a piece.
    
    Instead they have few countable valid moves. I didn't try to abstract
    this because it is very likely that a knight will never change the way
    it moves. So the way we calculate moves will never change. Hence, it
    doesn't need to be abstracted.

---
+++ lib/mod_move.rb
@@ -0,0 +1,26 @@
+# Calculate move for Knight and King which doesn't follow some pattern like Rook, Bishop
+module Move
+  def all_possible_moves(moves_dir, board)
+    moves_dir.map { |dir| calculate_move(board, dir) }.compact
+  end
+
+  # ------------------------------ PRIVATE METHODS -----------------------------
+  private
+
+  def calculate_move(board, dir)
+    row_index = position.row + dir[0]
+    col_index = position.col + dir[1]
+    move = [row_index, col_index]
+
+    position = Position.new(row_index, col_index)
+
+    return unless position.valid?
+
+    if board.contains_piece?(position)
+      piece = board.get_piece(position)
+      return if piece.color == color
+    end
+
+    move
+  end
+end
