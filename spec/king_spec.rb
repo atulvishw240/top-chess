54086b6c0e5585f445e7d9fdefb8c3d64e095d92
atulvishw240 <atulvishw240@gmail.com>
2025-08-11 09:31:04 +0530

    Create king_spec.rb

---
+++ spec/king_spec.rb
@@ -0,0 +1,31 @@
+require_relative "../lib/position"
+require_relative "../lib/board"
+require_relative "../lib/king"
+require_relative "../lib/bishop"
+
+describe King do
+  before(:each) do
+    @board = Board.new
+    @king = King.new("Black")
+    @bishop = Bishop.new("Black")
+    @bishop2 = Bishop.new("Black")
+  end
+
+  describe "#all_possible_moves" do
+    it "returns all possible moves for King at [3, 3]" do
+      @king.position = Position.new(3, 3)
+      poss_moves = [[2, 2], [2, 3], [2, 4], [3, 2], [3, 4], [4, 2], [4, 3], [4, 4]]
+      moves = @king.all_possible_moves(@board)
+      expect(moves).to eq(poss_moves)
+    end
+
+    it "returns all possible moves for King at [3, 3] blocked by his own piece at [3, 2] & [4, 3]" do
+      @king.position = Position.new(3, 3)
+      @board.update(@bishop, Position.new(3, 2))
+      @board.update(@bishop2, Position.new(4, 3))
+      poss_moves = [[2, 2], [2, 3], [2, 4], [3, 4], [4, 2], [4, 4]]
+      moves = @king.all_possible_moves(@board)
+      expect(moves).to eq(poss_moves)
+    end
+  end
+end
