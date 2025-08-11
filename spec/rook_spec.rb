812f381d213841d9bcf6341a6702da7d11e8d506
atulvishw240 <atulvishw240@gmail.com>
2025-08-11 12:44:30 +0530

    Add first test for Rook#all_possible_moves

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -11,4 +11,14 @@ describe Rook do
     @opp_bishop = Bishop.new("White")
     @rook = Rook.new("Black")
   end
+
+  describe "#all_possible_moves" do
+    it "returns all possible moves for rook at [4, 4]" do
+      @rook.position = Position.new(4, 4)
+      poss_moves = [[3, 4], [2, 4], [1, 4], [0, 4], [5, 4], [6, 4], [7, 4], [4, 5], [4, 6], [4, 7], [4, 3], [4, 2],
+                    [4, 1], [4, 0]]
+      moves = @rook.all_possible_moves(@board)
+      expect(moves).to eq(poss_moves)
+    end
+  end
 end
