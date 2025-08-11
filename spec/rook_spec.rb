535d9642ca44a1a50bd1f793e989ed6a5843d6f6
atulvishw240 <atulvishw240@gmail.com>
2025-08-11 12:53:52 +0530

    Add more tests for Rook#all_possible_moves

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -20,5 +20,23 @@ describe Rook do
       moves = @rook.all_possible_moves(@board)
       expect(moves).to eq(poss_moves)
     end
+
+    it "returns all possible moves for rook at [4, 4] blocked by own pieces at [5, 4] and [4, 5]" do
+      @rook.position = Position.new(4, 4)
+      @board.update(@bishop, Position.new(5, 4))
+      @board.update(@bishop2, Position.new(4, 5))
+      poss_moves = [[3, 4], [2, 4], [1, 4], [0, 4], [4, 3], [4, 2], [4, 1], [4, 0]]
+      moves = @rook.all_possible_moves(@board)
+      expect(moves).to eq(poss_moves)
+    end
+
+    it "returns all possible moves for rook at [4, 4] blocked by opponent piece at [6, 4]" do
+      @rook.position = Position.new(4, 4)
+      @board.update(@opp_bishop, Position.new(6, 4))
+      poss_moves = [[3, 4], [2, 4], [1, 4], [0, 4], [5, 4], [6, 4], [4, 5], [4, 6], [4, 7], [4, 3], [4, 2], [4, 1],
+                    [4, 0]]
+      moves = @rook.all_possible_moves(@board)
+      expect(moves).to eq(poss_moves)
+    end
   end
 end
