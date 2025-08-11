5a2aa5f1629341cb94b21d8945b6bbac02a48ace
atulvishw240 <atulvishw240@gmail.com>
2025-08-11 23:30:24 +0530

    Include the behavior of Move module in knight

--- pieces/king.rb
+++ pieces/king.rb
@@ -1,6 +1,9 @@
 require_relative "piece"
+require_relative "../lib/mod_move"
 # Represents King
 class King < Piece
+  include Move
+
   # [top_left, front, top_right, left, right, bottom_left, down, bottom_right]
   MOVES_DIR = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]].freeze
 
@@ -9,25 +12,6 @@ class King < Piece
   end
 
   def get_possible_moves(board)
-    MOVES_DIR.map { |dir| calculate_move(board, dir) }.compact
-  end
-
-  # ------------------------- PRIVATE METHODDS ---------------------------------------------
-
-  def calculate_move(board, dir)
-    row_index = position.row + dir[0]
-    col_index = position.col + dir[1]
-    move = [row_index, col_index]
-
-    position = Position.new(row_index, col_index)
-
-    return unless position.valid?
-
-    if board.contains_piece?(position)
-      piece = board.get_piece(position)
-      return if piece.color == color
-    end
-
-    move
+    all_possible_moves(MOVES_DIR, board)
   end
 end
--- pieces/knight.rb
+++ pieces/knight.rb
@@ -1,7 +1,16 @@
 require_relative "piece"
+require_relative "../lib/mod_move"
 # Represents Knight in Our Game
 class Knight < Piece
+  include Move
+
+  MOVES_DIR = [[-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1]].freeze
+
   def unicode
     "\u{265E}"
+  end
+
+  def get_possible_moves(board)
+    all_possible_moves(MOVES_DIR, board)
   end
 end
