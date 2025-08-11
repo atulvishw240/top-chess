eb379a2fdd525168001f9cc7901fdfe0be92e187
atulvishw240 <atulvishw240@gmail.com>
2025-08-11 22:32:46 +0530

    Queen can make a move and capture opponent pieces

--- pieces/queen.rb
+++ pieces/queen.rb
@@ -1,9 +1,23 @@
 require_relative "piece"
+require_relative "../lib/calculate_moves"
+require_relative "../lib/board"
 # Represents Queen in our Game
 class Queen < Piece
+  include CalcMoves
+
   MOVES_DIR = [[1, 1], [1, -1], [-1, 1], [-1, -1], [-1, 0], [1, 0], [0, 1], [0, -1]].freeze
 
   def unicode
     "\u{265B}"
+  end
+
+  def all_possible_moves(board)
+    moves = []
+
+    MOVES_DIR.each do |diagonal|
+      moves += calculate_moves_in_one_dir(board, diagonal)
+    end
+
+    moves
   end
 end
