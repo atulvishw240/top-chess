1ce68be8287efb5b2eccd4098362cd5075493d43
atulvishw240 <atulvishw240@gmail.com>
2025-08-15 08:02:36 +0530

    Add two ansi color constants

--- lib/ansi_codes.rb
+++ lib/ansi_codes.rb
@@ -3,4 +3,6 @@ module ANSI
   RESET_TERMINAL = "\e[0m".freeze
   WHITE_BACKGROUND = "\e[47m".freeze
   CYAN_BACKGROUND = "\e[48;5;45m".freeze
+  BLACK_FOREGROUND = "\e[30m".freeze
+  BROWN_FOREGROUND = "\e[38;5;160m".freeze
 end
