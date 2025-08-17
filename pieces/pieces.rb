c79f1fe9320e9d1a2a81ad6a7f34ea598115fe7b
atulvishw240 <atulvishw240@gmail.com>
2025-08-17 12:39:21 +0530

    Initialize pieces
    Pieces is a collection of piece objects. Pieces is a set to represent
    "Black" and "White" set of pieces in our game

--- pieces/pieces.rb
+++ pieces/pieces.rb
@@ -1,4 +1,15 @@
+require_relative "rook"
+require_relative "knight"
+require_relative "bishop"
+require_relative "queen"
+require_relative "king"
+require_relative "pawn"
+require_relative "../lib/position"
 # Reperesents Pieces which contains "Piece" objects
 class Pieces
   attr_accessor :pieces
+
+  def initialize(color)
+    @pieces = create_pieces(color)
+  end
 end
