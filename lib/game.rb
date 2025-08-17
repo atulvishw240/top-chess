f1d1079faf23b70035775a8b53787ae5d4c93e94
atulvishw240 <atulvishw240@gmail.com>
2025-08-17 17:21:46 +0530

    Add getters and setters for black and white pieces

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,18 @@
 # Contains Game Logic
 class Game
-  attr_reader :board, :black, :white
+  attr_reader :board
 
   def initialize(board, black, white)
     @board = board
     @black = black
     @white = white
+  end
+
+  def black
+    @black.pieces
+  end
+
+  def white
+    @white.pieces
   end
 end
