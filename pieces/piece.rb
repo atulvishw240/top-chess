d046aeeb02544c4fea31742ea449d032cf0b6676
atulvishw240 <atulvishw240@gmail.com>
2025-08-17 12:35:09 +0530

    Stop initializing a piece without position
    In our domain a piece without a position is useless. I tried to model
    the reality, because in reality a piece exist even without a position.

--- pieces/piece.rb
+++ pieces/piece.rb
@@ -5,16 +5,21 @@ class Piece
   include ANSI
 
   attr_accessor :position
-  attr_reader :color, :unicode
+  attr_reader :color
 
-  def initialize(color)
+  def initialize(color, position)
     @color = color
+    @position = position
     @unicode = unicode
   end
 
-  def unicode; end
+  def unicode
+    raise NotImplementedError, "All Pieces must implement unicode"
+  end
 
-  def get_possible_moves(board); end
+  def get_possible_moves(board)
+    raise NotImplementedError, "All Pieces must implement get_possible_moves"
+  end
 
   def to_s
     "#{color}#{unicode} #{RESET_TERMINAL}"
