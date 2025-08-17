a8d7a87c41386631e77559d586ee6b003798192d
atulvishw240 <atulvishw240@gmail.com>
2025-08-17 17:30:58 +0530

    Add board setup code to Game
    Setting up board is game's responsibility

--- lib/game.rb
+++ lib/game.rb
@@ -8,6 +8,18 @@ class Game
     @white = white
   end
 
+  def setup
+    setup_pieces_on_board(black)
+    setup_pieces_on_board(white)
+    board.display
+  end
+
+  def setup_pieces_on_board(set)
+    set.each do |piece|
+      board.update(piece, piece.position)
+    end
+  end
+
   def black
     @black.pieces
   end
--- main.rb
+++ main.rb
@@ -1,8 +1,9 @@
-require_relative "lib/board"
-require_relative "lib/ansi_codes"
 require_relative "pieces/pieces"
+require_relative "lib/game"
 
+board = Board.new
 black = Pieces.new("\e[30m")
 brown = Pieces.new("\e[38;5;160m")
-board = Board.new(black.pieces, brown.pieces)
-board.display
+game = Game.new(board, black, brown)
+
+game.setup
