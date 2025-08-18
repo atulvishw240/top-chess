e4cef7848e12710173817925c4f920dcb6ea41db
atulvishw240 <atulvishw240@gmail.com>
2025-08-18 16:23:09 +0530

    Rename ansi_codes.rb to constants.rb

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,5 +1,5 @@
 # Contains all the ansi codes used throughout the program
-module ANSI
+module Constants
   RESET_TERMINAL = "\e[0m".freeze
   WHITE_BACKGROUND = "\e[47m".freeze
   CYAN_BACKGROUND = "\e[48;5;45m".freeze
