1d94d0f0f8851161c9e5ca4fa0a8bd63d4c74bb2
atulvishw240 <atulvishw240@gmail.com>
2025-08-20 11:20:45 +0530

    Add tests for Board#contains_piece?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,6 +5,7 @@ describe Board do
   before(:each) do
     @board = Board.new
   end
+
   describe "#get_piece(position)" do
     it "returns the piece at position" do
       position = Position.new(3, 3)
@@ -13,6 +14,21 @@ describe Board do
 
       piece = @board.get_piece(position)
       expect(piece).to eq(@bishop)
+    end
+  end
+
+  describe "#contains_piece?(pos)" do
+    it "returns true if there's a piece at pos" do
+      position = Position.new(3, 3)
+      @bishop = Bishop.new("Black", position)
+      @board.update(@bishop)
+
+      expect(true).to eq(@board.contains_piece?(position))
+    end
+
+    it "returns false if there isn't a piece at pos" do
+      position = Position.new(3, 3)
+      expect(false).to eq(@board.contains_piece?(position))
     end
   end
 end
