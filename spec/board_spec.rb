cddc3e436dd5ac5f965c865da36b90234d29de0a
atulvishw240 <atulvishw240@gmail.com>
2025-08-20 11:16:18 +0530

    Add test for Board#get_piece(pos)

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,3 +1,18 @@
 require_relative "../lib/board"
+require_relative "../lib/position"
+require_relative "../pieces/bishop"
 describe Board do
+  before(:each) do
+    @board = Board.new
+  end
+  describe "#get_piece(position)" do
+    it "returns the piece at position" do
+      position = Position.new(3, 3)
+      @bishop = Bishop.new("Black", position)
+      @board.update(@bishop)
+
+      piece = @board.get_piece(position)
+      expect(piece).to eq(@bishop)
+    end
+  end
 end
