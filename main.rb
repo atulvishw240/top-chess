8696318a8630211bb2a1877f98ac9ceb3f72e249
atulvishw240 <atulvishw240@gmail.com>
2025-08-23 00:04:46 +0530

    Update main.rb

--- main.rb
+++ main.rb
@@ -2,11 +2,12 @@ require_relative "pieces/pieces"
 require_relative "lib/game"
 require_relative "lib/player"
 
-board = Board.new
 black = Pieces.new("\e[30m")
 brown = Pieces.new("\e[38;5;160m")
-player1 = Player.new("Atul", black)
-player2 = Player.new("Gaurav", brown)
+pieces = black.pieces.concat(brown.pieces)
+board = Board.new(pieces)
+player1 = Player.new("Gaurav", "\e[38;5;160m")
+player2 = Player.new("Atul", "\e[30m")
 game = Game.new(board, player1, player2)
 
 game.play
