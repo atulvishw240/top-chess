384e6e14f8f86943c9fbc05764867742a18a65cf
atulvishw240 <atulvishw240@gmail.com>
2025-09-03 18:33:35 +0530

    Only select pieces which gets us out of check
    When you're in check your pieces selections gets limited. The piece
    moves get limited as well.
    
    The hard part in implementing this was that I was only considering only
    "escaping" as a way to get out of the check. But we can capture the
    checking piece to get out of check too.

--- lib/game.rb
+++ lib/game.rb
@@ -12,32 +12,79 @@ class Game
     setup_pieces_on_board
     board.display
     loop do
+      p check?
+
+      # Get all pieces of current player
       pieces = pieces_set(current_player.color)
-      pieces.reject! { |piece| piece.get_possible_moves(board).empty? }
-      selections = pieces.map do |piece|
-        [piece.position.row, piece.position.col]
+      # FOR each piece
+      selections = []
+      pieces.each do |piece|
+        #   Calculate all possible moves
+        moves = piece.get_possible_moves(board)
+        #   Select only those moves which gets us out of check
+        filtered_moves = filter_moves_that_does_not_remove_check(piece, moves)
+        selections << [piece.position.row, piece.position.col] unless filtered_moves.empty?
       end
+
+      #   If you get a non-empty set in previous step then add this piece to "set of possible selections"
+      # ENDFOR
+      # return "set of possible selections"
+
+      # pieces = pieces_set(current_player.color)
+      # pieces.reject! { |piece| piece.get_possible_moves(board).empty? }
+      # selections = pieces.map do |piece|
+      #   [piece.position.row, piece.position.col]
+      # end
 
       p selections
 
       puts ""
-      p check?
       selection = current_player.select_piece(selections)
       position = Position.new(selection[0], selection[1])
       piece = board.get_piece(position)
 
-      moves = piece.get_possible_moves(board)
-      p moves
-
-      move = current_player.select_move(moves)
-      new_position = Position.new(move[0], move[1])
-      board.delete_piece(new_position) if capture?(new_position)
-
-      board.update(piece, new_position)
+      move = make_move(piece)
+      update_state_on_move(piece, move)
 
       board.display
       switch_players!
     end
+  end
+
+  #   Select only those moves which gets us out of check
+  def filter_moves_that_does_not_remove_check(piece, moves)
+    prev_position = piece.position
+    filtered_moves = []
+    moves.each do |move|
+      new_position = Position.new(move[0], move[1])
+      # If this new_position contains piece then
+      # Set the coordinate of this piece to -1, -1 (imaginary)
+      if board.contains_piece?(new_position)
+        opp_piece = board.get_piece(new_position)
+        opp_piece.position = Position.new(-1, -1)
+      end
+
+      piece.position = new_position
+      filtered_moves << move unless check?
+      piece.position = prev_position
+      opp_piece.position = new_position if board.contains_piece?(new_position)
+    end
+
+    filtered_moves
+  end
+
+  def make_move(piece)
+    moves = piece.get_possible_moves(board)
+    p moves
+
+    move = current_player.select_move(moves)
+    Position.new(move[0], move[1])
+  end
+
+  def update_state_on_move(piece, move)
+    board.delete_piece(move) if capture?(move)
+
+    board.update(piece, move)
   end
 
   def check?
