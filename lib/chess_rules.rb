9158a740069370f029d99199cec8ee10ecaca9fa
Atul Vishwakarma <atulvishw240@gmail.com>
2025-09-11 20:23:46 +0530

    Create check? method in ChessRules

--- lib/chess_rules.rb
+++ lib/chess_rules.rb
@@ -1,3 +1,17 @@
+require_relative '../modules/constants'
 # Contains game logic for our chess
 class ChessRules
+  include Constants
+
+  def check?(board, color)
+    pieces = board.pieces_set(opponent_color(color))
+    moves = []
+    pieces.each { |piece| moves += piece.get_possible_moves(board) }
+    king = board.king(color)
+    moves.any? { |move| move == king.position }
+  end
+
+  def opponent_color(color)
+    color == BLACK_FOREGROUND ? BROWN_FOREGROUND : BLACK_FOREGROUND
+  end
 end
