41ff820f2fb225b9ec17ba6785bef48036a59d4a
Atul Vishwakarma <atulvishw240@gmail.com>
2025-09-12 16:35:27 +0530

    Add codes (ansi and uni) for markers can captures

--- modules/constants.rb
+++ modules/constants.rb
@@ -5,5 +5,7 @@ module Constants
   CYAN_BACKGROUND = "\e[48;5;45m".freeze
   BLACK_FOREGROUND = "\e[30m".freeze
   BROWN_FOREGROUND = "\e[38;5;160m".freeze
+  CAPTURE = "\e[48;5;47m".freeze
+  MARKER = "\e[90m\u{25CF}".freeze
   EMPTY = '  '.freeze
 end
