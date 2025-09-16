2e26bdcdc1203f065b775215014a277054e7f22c
Atul Vishwakarma <atulvishw240@gmail.com>
2025-09-16 18:49:23 +0530

    Refactor Game
    Delete redundant methods

--- lib/game.rb
+++ lib/game.rb
@@ -43,7 +43,7 @@ class Game
       rules.update_game_state(board, piece, move)
 
       if rules.promotion?(board, current_player.color)
-        choice = promote_to
+        choice = ui.promote_to
         board.delete_piece(move)
         promoted_piece = piece_factory(choice, current_player.color, move)
         board.add_piece(promoted_piece)
@@ -54,63 +54,6 @@ class Game
     end
 
     puts "The #{opponent_player.name} won the game!!"
-  end
-
-  def select_piece(selections)
-    puts 'Enter the coordinates of a piece which you want to select.'
-    selection = current_player.select(selections)
-    position = to_position_object(selection)
-    board.get_piece(position)
-  end
-
-  def display_markers_and_captures(moves)
-    moves.each do |move|
-      if board.contains_piece?(move)
-        board.assign_color_to_square(move, Constants::CAPTURE)
-      else
-        board.update(Constants::MARKER, move)
-      end
-    end
-  end
-
-  def clean_markers_and_captures(moves)
-    moves.each do |move|
-      if board.contains_piece?(move)
-        color = color(move)
-        board.assign_color_to_square(move, color)
-      else
-        board.update(Constants::EMPTY, move)
-      end
-    end
-  end
-
-  def color(position)
-    sum = position.row + position.col
-
-    if sum.even?
-      WHITE_BACKGROUND
-    else
-      CYAN_BACKGROUND
-    end
-  end
-
-  def select_move(moves)
-    puts 'Enter the coordinates where you would like to move your piece.'
-    move = current_player.select(moves)
-    to_position_object(move)
-  end
-
-  def promote_to
-    puts 'Promote your pawn to:'
-    puts '1) Queen'
-    puts '2) Rook'
-    puts '3) Bishop'
-    puts '4) Knight'
-
-    puts 'Enter your choice (integer): '
-    choice = gets.chomp.to_i
-
-    choice if choice.between?(1, 4)
   end
 
   def piece_factory(integer, color, position)
@@ -140,14 +83,5 @@ class Game
 
   def switch_players!
     @current_player_id = 1 - @current_player_id
-  end
-
-  # Convert d1 to Position.new(1, 4)
-  def to_position_object(standard_form)
-    coordinates = standard_form.chars
-    row = to_row_index(coordinates[1].to_i)
-    col = to_col_index(coordinates[0])
-
-    Position.new(row, col)
   end
 end
