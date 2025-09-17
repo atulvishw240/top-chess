0dc24b3ccbdbe32f7f7c193115155136a1360bf2
Atul Vishwakarma <atulvishw240@gmail.com>
2025-09-17 17:09:25 +0530

    Separate Promotion logic from Game#play
    This commit take all promotion related code from Game and puts it under
    Promotion class

--- lib/promotion.rb
+++ lib/promotion.rb
@@ -13,4 +13,43 @@ class Promotion
 
     false
   end
+
+  def update_state(board, piece, move)
+    choice = promote_to
+    promoted_piece = piece_factory(choice, piece.color, move)
+
+    # Update state to reflect promotion
+    board.delete_piece(move)
+    board.add_piece(promoted_piece)
+    board.update(promoted_piece, move)
+  end
+
+  def promote_to
+    puts 'Promote your pawn to:'
+    puts '1) Queen'
+    puts '2) Rook'
+    puts '3) Bishop'
+    puts '4) Knight'
+
+    puts 'Enter your choice (integer): '
+    choice = gets.chomp.to_i
+
+    return choice if choice.between?(1, 4)
+
+    puts 'Please! Enter a valid input'
+    promote_to
+  end
+
+  def piece_factory(integer, color, position)
+    case integer
+    when 1
+      Queen.new(color, position)
+    when 2
+      Rook.new(color, position)
+    when 3
+      Bishop.new(color, position)
+    when 4
+      Knight.new(color, position)
+    end
+  end
 end
